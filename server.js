b40af28ac916a6482c0b2f2c799382ad9f36c53b
nladha09 <ladha.noreen@gmail.com>
2019-01-09 14:41:09 -0700

    added item & character stats

--- server.js
+++ server.js
@@ -16,27 +16,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Handlebars
-// app.engine(
-//   "handlebars",
-//   exphbs({
-//     defaultLayout: "main"
-//   })
-// );
-// app.set("view engine", "handlebars");
 
 // // Routes
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
 
-// var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
+// Starting the server, syncing our models
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log(
