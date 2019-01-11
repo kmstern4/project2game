744df2bff0df19faa0f4747cff46200eebbff5c3
Ryan <ryan.ryanstocke@gmail.com>
2019-01-11 13:54:16 -0700

    working auth

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,56 @@
-var db = require("../models");
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  // Load index page
+
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    // If the user already has an account send them to the members page
+    // we could also redirect them to play the game on the index
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // app.get("/game", function(req, res) {
+  //   // If the user already has an account send them to the game page
+  //   // we could also redirect them to play the game on the index
+  //   if (req.user) {
+  //     res.redirect("/game");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/game.html"));
+  // });
+  app.get("/signup", function(req, res) {
+    // If the user already has an account send them to the members page
+    // we could also redirect them to play the game on the index
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+  // app.get("/members", function(req, res) {
+  //   // If the user already has an account send them to the members page
+  //   if (req.user) {
+  //     res.redirect("/members");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/members.html"));
+  // });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+  app.get("/game",isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/game.html"));
   });
 };
--- server.js
+++ server.js
@@ -1,34 +1,43 @@
 require("dotenv").config();
 var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 3000;
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+
+// Setting up port and requiring models for syncing
+var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 var db = require("./models");
 
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+var app = express();
+app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use(express.static("public"));
+//We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
 
-// Routes
+// Routes/Requiring our routes
+require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
 
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models
-// db.sequelize.sync().then(function() {
+// Syncing our database and logging a message to the user upon success
+//this will listen to and show all activities on our terminal to 
+//let us know what is happening in our app
 
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+  db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+      console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    });
   });
-// });
 
 module.exports = app;
