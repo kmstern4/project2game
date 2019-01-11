91059a78fa61bce2d059eb5a845d37c4e34b2e51
Kelly Stern <kmstern4@gmail.com>
2019-01-11 14:49:36 -0700

    added container text and walking tweens

--- public/game.js
+++ public/game.js
@@ -1,3 +1,4 @@
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -22,6 +23,9 @@ var farmzombie;
 var keydown = false;
 var playerhealth = 10;
 var farmzombiehealth = 10;
+var textcontainer;
+var textbox;
+var text;
 
 var game = new Phaser.Game(config);
 
@@ -29,12 +33,52 @@ function preload() {
     this.load.atlas("hoodgirl", "assets/hoodgirl.png", "assets/hoodgirl.json");
     this.load.atlas("farmzombie", "assets/farmzombie.png", "assets/farmzombie.json");
     this.load.image("volcano", "assets/volcano.png");
+    this.load.image("textbox", "assets/600wguibox.png");
 }
 
 function create() {
     this.add.image(400, 300, "volcano");
-    player = this.add.sprite(100, 450, "hoodgirl", "idle001.png");
-    farmzombie = this.add.sprite(600, 450, "farmzombie", "idle001.png");
+    textbox = this.add.image(0, 0, "textbox");
+    text = this.add.text(-100, -10, "Farmer zombie has died");
+    text.visible = false;
+    player = this.add.sprite(-100, 450, "hoodgirl", "idle001.png");
+    farmzombie = this.add.sprite(900, 450, "farmzombie", "idle001.png");
+
+// setting the text container
+    textcontainer = this.add.container(400, 200, textbox);
+    textcontainer.visible = false;
+
+    textcontainer.setSize(400, 100);
+
+    textcontainer.setInteractive();
+
+    textcontainer.on("pointerover", function() {
+        textbox.setTint(0x44ff44);
+    });
+
+    textcontainer.on("pointerout", function() {
+        textbox.clearTint();
+    });
+
+
+// Tweens
+    tween = this.tweens.add({
+        targets: player,
+        x: 200,
+        ease: "power1",
+        duration: 2500,
+        repeat: 0
+    });
+
+    ztween = this.tweens.add({
+        targets: farmzombie,
+        x: 600,
+        ease: "power1",
+        duration: 2500,
+        repeat: 0
+    });
+
+
 
 // Player animation creations
     this.anims.create({
@@ -85,6 +129,19 @@ function create() {
         repeat: 0
     });
 
+    this.anims.create({
+        key: "hgwalking",
+        frames: this.anims.generateFrameNames("hoodgirl", {
+            prefix: "walking00",
+            suffix: ".png",
+            start: 1,
+            end: 24
+        }),
+        frameRate: 20,
+        repeat: 1
+    });
+
+
 // Zombie animation creations
     this.anims.create({
         key: "fzidle",
@@ -134,9 +191,22 @@ function create() {
         repeat: 0
     });
 
+    this.anims.create({
+        key: "fzwalking",
+        frames: this.anims.generateFrameNames("farmzombie", {
+            prefix: "walking00",
+            suffix: ".png",
+            start: 1,
+            end: 18
+        }),
+        frameRate: 20,
+        repeat: 2
+    });
+
+
 // when scene loads start playing idle animations for player and zombie
-    player.play("hgidle");
-    farmzombie.play("fzidle");
+    player.play("hgwalking");
+    farmzombie.play("fzwalking");
 
     player.on("animationcomplete", function() {
         player.play("hgidle");
@@ -145,6 +215,9 @@ function create() {
     farmzombie.on("animationcomplete", function() {
         console.log(this.anims.currentAnim.key);
         if (this.anims.currentAnim.key == "fzdying") {
+            textcontainer.visible = true;
+            textcontainer.add(text);
+            text.visible = true;
             this.anims.pause();
         } else {
             farmzombie.play("fzidle");
